0ff6333156cb66d955a333c20d1b1c1ff7b6f986
Tomas Cornara <55905365+TomasCornara@users.noreply.github.com>
2025-05-23 10:42:42 -0300

    Comentario de la consigna

--- scripts/pruebasSPSocio.sql
+++ scripts/pruebasSPSocio.sql
@@ -1,3 +1,41 @@
+/*
+	Entrega 4 - Documento de instalación y configuración
+
+	Trabajo Practico DDBBA Entrega 3 - Grupo 1
+	Comision 5600 - Viernes Tarde 
+	43990422 | Aguirre, Alex Rubén 
+	45234709 | Gauto, Gastón Santiago 
+	44363498 | Caballero, Facundo 
+	40993965 | Cornara Perez, Tomás Andrés
+
+		Luego de decidirse por un motor de base de datos relacional, llegó el momento de generar la
+	base de datos. En esta oportunidad utilizarán SQL Server.
+	Deberá instalar el DMBS y documentar el proceso. No incluya capturas de pantalla. Detalle
+	las configuraciones aplicadas (ubicación de archivos, memoria asignada, seguridad, puertos,
+	etc.) en un documento como el que le entregaría al DBA.
+	Cree la base de datos, entidades y relaciones. Incluya restricciones y claves. Deberá entregar
+	un archivo .sql con el script completo de creación (debe funcionar si se lo ejecuta “tal cual” es
+	entregado en una sola ejecución). Incluya comentarios para indicar qué hace cada módulo
+	de código.
+	Genere store procedures para manejar la inserción, modificado, borrado (si corresponde,
+	también debe decidir si determinadas entidades solo admitirán borrado lógico) de cada tabla.
+	Los nombres de los store procedures NO deben comenzar con “SP”.
+	Algunas operaciones implicarán store procedures que involucran varias tablas, uso de
+	transacciones, etc. Puede que incluso realicen ciertas operaciones mediante varios SPs.
+	Asegúrense de que los comentarios que acompañen al código lo expliquen.
+	Genere esquemas para organizar de forma lógica los componentes del sistema y aplique esto
+	en la creación de objetos. NO use el esquema “dbo”.
+	Todos los SP creados deben estar acompañados de juegos de prueba. Se espera que
+	realicen validaciones básicas en los SP (p/e cantidad mayor a cero, CUIT válido, etc.) y que
+	en los juegos de prueba demuestren la correcta aplicación de las validaciones.
+	Las pruebas deben realizarse en un script separado, donde con comentarios se indique en
+	cada caso el resultado esperado
+	El archivo .sql con el script debe incluir comentarios donde consten este enunciado, la fecha
+	de entrega, número de grupo, nombre de la materia, nombres y DNI de los alumnos.
+	Entregar todo en un zip (observar las pautas para nomenclatura antes expuestas) mediante
+	la sección de prácticas de MIEL. Solo uno de los miembros del grupo debe hacer la entrega.
+*/
+
 -- Pruebas Socio
 -- TEST STORED PROCEDURE CrearSocio:
 
